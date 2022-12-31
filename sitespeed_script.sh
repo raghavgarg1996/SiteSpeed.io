950434285a177b1a465b766a71a351c198ea9606
Raghav.Garg <raghav.garg@ril.com>
2022-12-31 12:12:17 +0530

    Adding mobile cases

--- sitespeed_script.sh
+++ sitespeed_script.sh
@@ -2,4 +2,4 @@
 
 docker run --shm-size=1g --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io --outputFolder sitespeed-result/Desktop/ https://www.marvel.com/movies -b chrome
 
-docker run --shm-size=1g --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io --outputFolder Users/raghav.garg/Documents/Raghav/Technology/SiteSpeed/sitespeed-result/Mobile/ https://www.marvel.com/movies -b chrome --mobile
+docker run --shm-size=1g --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io --outputFolder sitespeed-result/Mobile/ https://www.marvel.com/movies -b chrome --mobile
